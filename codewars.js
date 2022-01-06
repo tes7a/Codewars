1f4b5ffa5e7ab6b86cd5e9b0d7616bcb847e784a
Kostya <ttes7a@gmail.com>
2022-01-07 00:14:10 +0200

    kata 8qu

--- codewars.js
+++ codewars.js
@@ -58,3 +58,13 @@ function finalGrade (exam, projects) {
     if (exam > 50 && projects >= 2) return 75;
     return 0;
 }
+
+//3
+
+//Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+const smash = (words) => words.join(" ");
