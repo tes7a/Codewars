8745490a77cbde97cd99a574ab95c753a0551306
tes7a <ttes7a@gmail.com>
2022-01-06 17:20:41 +0200

    done kata

--- codewars.js
+++ codewars.js
@@ -1,3 +1,5 @@
+//1
+
 // Let's play! You have to return which player won! In case of a draw return Draw!.
 //Examples:
 //
@@ -5,7 +7,6 @@
 // rps('scissors','rock') // Player 2 won!
 // rps('paper','paper') // Draw!
 
-//1
 const rps = (p1, p2) => {
     if (p1 === 'scissors' && p2 === 'paper') {
         return 'Player 1 won!'
@@ -38,3 +39,22 @@ const rps = (p1, p2) => {
 //     }
 // };
 
+//2
+
+//Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+//
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+//
+// This function should return a number (final grade). There are four types of final grades:
+//
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+
+function finalGrade (exam, projects) {
+    if (exam > 90 || projects > 10) return 100;
+    if (exam > 75 && projects >= 5) return 90;
+    if (exam > 50 && projects >= 2) return 75;
+    return 0;
+}
