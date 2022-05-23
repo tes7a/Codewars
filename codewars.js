fa9cdac160f2899339d9b1f647f2e2ffaee6df73
tes7a <ttes7a@gmail.com>
2022-05-23 15:26:41 +0300

    18, 19

--- codewars.js
+++ codewars.js
@@ -320,3 +320,32 @@ function howMuchWater(water, load, clothes){
 }
 
 howMuchWater(10,11,20)
+
+//19
+
+//Simple interest on a loan is calculated by simply taking the initial amount (the principal, p) and multiplying it by a rate of interest (r) and the number of time periods (n).
+//
+// Compound interest is calculated by adding the interest after each time period to the amount owed, then calculating the next interest payment based on the principal PLUS the interest from all previous periods.
+//
+// Given a principal p, interest rate r, and a number of periods n, return an array [ total owed under simple interest, total owed under compound interest ]
+//
+// Notes:
+//
+// Round all answers to the nearest integer
+// Principal will always be an integer between 0 and 9999
+// interest rate will be a decimal between 0 and 1
+// number of time periods will be an integer between 0 and 50
+
+//P * (1 + r * n)
+//P * (1 + r)^n
+
+function interest(P,r,n) {
+    const arr = [];
+    arr.push(Math.round(P * (1 + r * n)));
+    arr.push(Math.round(P * Math.pow(1 + r, n)));
+    console.log(arr);
+
+    return arr
+}
+
+interest(100,0.1,10)
